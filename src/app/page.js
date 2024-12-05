5a957665e3f5396484a30d65eb1874237614759f
tarasowski <dimitri+github@tarasowski.de>
2024-12-05 10:55:38 +0100

    fix: headline changed

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-    <h1 class="font-bold text-3xl">Hello from Techstarter.js</h1>
+    <h1 class="font-bold text-3xl">
+    Hello this is Dimi
+    </h1>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
